150633f97f3b3f6efa9610e48c49d54cc0b14451
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-31 17:41:17 -0400

    build pages/routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import useGlobalReducer from '../hooks/useGlobalReducer.jsx';
 
+import { Contact } from './Contact.jsx';
+
 export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
 
   return (
     <div className="text-center mt-5">
       <h1>Contact List</h1>
+      <Contact />
     </div>
   );
 };
