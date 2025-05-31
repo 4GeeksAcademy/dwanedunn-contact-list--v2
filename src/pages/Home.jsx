da0a51628bb58cbc458415609660f14d7632d0a1
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-31 18:20:38 -0400

    build the fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import useGlobalReducer from '../hooks/useGlobalReducer.jsx';
 
 import { Contact } from './Contact.jsx';
 
 export const Home = () => {
   const { store, dispatch } = useGlobalReducer();
+
+  const fetchContacts = async () = {
+	try {
+		const response = await fetch(https://playground.4geeks.com/contact/agendas/dwane_dunn/contacts);
+		const resultData = await response.json();
+		dispatch({});
+	} catch (error) {
+		throw new Error(error);
+	}
+  }
+
+  useEffect(()=> {
+	fetchContacts()
+  },[])
+
 
   return (
     <div className="text-center mt-5">
